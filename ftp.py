2f3035c5a6ad4782c32015fe4c8b843b523778d5
r0mk <r0mk@r0mk.ru>
2018-09-25 17:26:24 +0400

    add pages

--- ftp.py
+++ ftp.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+#pip3 install pymongo
 
 from ftplib import FTP
 ftp = FTP('78.85.35.103')
@@ -16,18 +17,29 @@ url="http://theautomd.com/", # Your store URL
 consumer_key="ck_a00ad9d6c10d9e8d8a251d5dcc9b5d1887f230c2", # Your consumer key
 consumer_secret="cs_489fc080e4232a40ca616ec52341fb6a1467ba64", # Your consumer secret
 wp_api=True, # Enable the WP REST API integration
+timeout=10,
 version="wc/v2" # WooCommerce WP REST API version
 )
 #print(wcapi.get("").json())
-#data = {
-#        id: 219
-#        }
 #Retrive one product
-resp = (wcapi.get("products").json())
-#print(type(resp))
+#
+data = {
+        'page': 1,
+        'per_page': 1
+        }
+#
+resp = (wcapi.get("products?per_page=10").headers)
+print(type(resp))
+print("Total pages " + resp['X-WP-TotalPages'])
 for every in resp:
+    print(every['id'])
     #print(every['name'])
-    print(every['name'])
+
+from pymongo import MongoClient
+client = MongoClient()
+
+db = client.prodlist
+
 #Retrive all products
 #print(wcapi.get("products").json())
 
