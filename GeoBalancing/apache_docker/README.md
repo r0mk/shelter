48639d46bdacb0c2d48f31af9319507aa301010a
root <root@Domik-PC1.Domik>
2019-04-04 17:02:18 +0400

    add docker files

---
+++ GeoBalancing/apache_docker/README.md
@@ -0,0 +1,3 @@
+apache_hostname
+===============
+This container runs a simple apache server that display the hostname of container in which it is running.
---
+++ GeoBalancing/apache_docker/index.php
@@ -0,0 +1,11 @@
+<html>
+	<body>
+                This page is displayed on the container: <b>
+<?php
+$hostname = gethostname();
+print $hostname;
+?>
+</b>
+	</body>
+</html>
+
